1f490740e3a5dbe22a9256a2cb371dc93990bcf1
jamesbearman <jaimepardo66@gmail.com>
2026-07-27 01:10:47 -0400

    Estructura de los formularios

--- resources/views/proyectos/crear.blade.php
+++ resources/views/proyectos/crear.blade.php
@@ -3,49 +3,52 @@
     @csrf
     <h2>Crea Tu Proyecto!</h2>
 
-    <label for="Nombre">Nombre del Proyecto:</label>
-    <input 
-        type="text" 
-        id="Nombre" 
-        name="Nombre" 
-        required
-    >
+    <div>
+        <label for="Nombre">Nombre del Proyecto:</label>
+        <input 
+            type="text" 
+            id="Nombre" 
+            name="Nombre" 
+            required
+        >
+    </div>
 
-    <label for="Fecha_de_inicio">Fecha de Inicio:</label>
-    <input 
-        type="date" 
-        id="Fecha_de_inicio" 
-        name="Fecha_de_inicio" 
-        required
-    >
-
-    <label for="Estado">Estado del Proyecto:</label>
-    <input 
+    <div>
+        <label for="Fecha_de_inicio">Fecha de Inicio:</label>
+        <input 
+            type="date" 
+            id="Fecha_de_inicio" 
+            name="Fecha_de_inicio" 
+            required
+        >
+    </div>
+    <div>
+        <label for="Estado">Estado del Proyecto:</label>
+        <input 
             type="text" 
             id="Estado" 
             name="Estado" 
             required
-    >
-
-    <label for="Responsable">Responsable del Proyecto:</label>
-    <input 
+        >
+    </div>
+    <div>
+        <label for="Responsable">Responsable del Proyecto:</label>
+        <input 
             type="text" 
             id="Responsable" 
             name="Responsable" 
             required
-    >
-
-    <label for="Monto">Monto del Proyecto:</label>
-    <input 
+        >
+    </div>
+    <div>
+        <label for="Monto">Monto del Proyecto:</label>
+        <input 
             type="number" 
             id="Monto" 
             name="Monto" 
             required
-    >
-
-
-  <button type="submit">Crear Proyecto</button>
-
-
+        >
+    </div>
+    <button type="submit">Crear Proyecto</button>
     </form>
 </x-layout>
