8deb17024e3a8c15e32edbb9b608566efd4f7745
jamesbearman <jaimepardo66@gmail.com>
2026-07-27 01:13:38 -0400

    Contenido de la pagina de inicio

--- resources/views/proyectos/index.blade.php
+++ resources/views/proyectos/index.blade.php
@@ -1,4 +1,7 @@
 <x-layout>
-    <h1>INDEX!</h1>
-    Bienvenido!
+    <div>
+        <h1>INDEX! GESTION DE PROYECTOS!</h1>
+        <p>Bienvenido! En esta pestaña puedes crear, ver, actualizar y eliminar proyectos!!11!!1</p>
+        <a href="{{route('proyectos.lista')}}">Ver todos los proyectos!!!!!!</a>
+    </div>
 </x-layout>
