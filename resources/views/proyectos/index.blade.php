9f38cf1e35c1f36547cc37de0daba4800c15c958
jamesbearman <jaimepardo66@gmail.com>
2026-07-27 02:57:34 -0400

    Degradado en pagina de inicio

--- resources/views/proyectos/index.blade.php
+++ resources/views/proyectos/index.blade.php
@@ -1,7 +1,10 @@
 <x-layout>
-    <div>
-        <h1>INDEX! GESTION DE PROYECTOS!</h1>
-        <p>Bienvenido! En esta pestaña puedes crear, ver, actualizar y eliminar proyectos!!11!!1</p>
-        <a href="{{route('proyectos.lista')}}">Ver todos los proyectos!!!!!!</a>
+    <div class="bg-white shadow-md rounded-xl p-8 text-center">
+        <h1 class="text-2xl font-bold mb-4">INDEX! GESTION DE PROYECTOS!</h1>
+        <p class="text-slate-600 mb-6">Bienvenido! En esta pestaña puedes crear, ver, 
+        actualizar y eliminar proyectos!!11!!1</p>
+        <a href="{{route('proyectos.lista')}}" class="inline-block px-4 py-2 bg-blue-500 text-white font-medium rounded-md hover:bg-blue-600">
+            Ver todos los proyectos!!!!!!
+        </a>
     </div>
 </x-layout>
