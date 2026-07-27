d28048ac5edc00560d53af98af1fe4e0a8b1ae54
jamesbearman <jaimepardo66@gmail.com>
2026-07-27 01:02:50 -0400

    Estructura del nav para el css

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,7 +5,7 @@
     <title>Proyectos</title>
      @vite('resources/css/app.css')
 </head>
-<body>
+<body class="bg-state-100 text-gray-800 min-h-screen">
     @if(session('success'))
         <div>
             {{session('success')}}
@@ -14,11 +14,13 @@
     
     <header>
         <nav>
-            <h1>Proyectos Ficticios! Eva 1</h1>
-            <a href="{{ route('proyectos.index')}}">Inicio!</a>
-            <a href="{{ route('proyectos.lista')}}">Todos los proyectos!</a>
-            <a href="{{route('proyectos.crear')}}">Crea un proyecto!</a>
-            <x-api-uf></x-api-uf>
+            <div>
+                <h1>Proyectos Ficticios! Eva 1</h1>
+                <a href="{{ route('proyectos.index')}}">Inicio!</a>
+                <a href="{{ route('proyectos.lista')}}">Todos los proyectos!</a>
+                <a href="{{route('proyectos.crear')}}">Crea un proyecto!</a>
+                <x-api-uf></x-api-uf>
+            </div>
         </nav>
     </header>
 
