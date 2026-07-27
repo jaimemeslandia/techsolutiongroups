ae53459a2ac656b8046d540a4ac92a75fe1fc319
jamesbearman <jaimepardo66@gmail.com>
2026-07-27 02:24:57 -0400

    Degradados en tarjetas y botones de accion

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,4 +1,4 @@
-<div>
+<div class="bg-white border rounded-xl shadow-md p-4 hover:shadow-lg transition">
     {{ $slot }}
-    <a href="{{ $attributes->get('href')}}">Ver Detalles</a>
+    <a href="{{ $attributes->get('href')}}" class="inline-block mt-2 text-sm font-medium text-orange-600 hover:text-red-700">Ver Detalles</a>
 </div>
--- resources/views/components/upd-delete.blade.php
+++ resources/views/components/upd-delete.blade.php
@@ -1,12 +1,22 @@
 
-<div>
+<div class="flex gap-3 mt-4">
     <ul>
         <li>
-            <a href="{{route("proyectos.actualizar", $attributes->get('id'))}}">Actualizar proyecto!</a>
+            <a href="{{route("proyectos.actualizar", $attributes->get('id'))}}" 
+                class="inline-block px-3 py-2 rounded-md
+                text-sm font-medium text-white transition bg-linear-to-tr
+                 from-yellow-500 to-red-500 hover:to-red-600">
+                Actualizar proyecto!
+                </a>
         </li>
 
         <li>
-            <a href="{{route("proyectos.borrar", $attributes->get('id'))}}">Borrar Proyecto!</a>
+            <a href="{{route("proyectos.borrar", $attributes->get('id'))}}" 
+                class="inline-block px-3 py-2 rounded-md
+                text-sm font-medium text-white transition bg-linear-to-tr
+                 from-red-600 to-red-900 hover:to-red-700">
+                Borrar Proyecto!
+            </a>
         </li>
     </ul>
 </div>
